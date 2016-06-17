1192f08841215e38941ad735b90fc0cfa787d994
David Poliakoff <david.poliakoff@gmail.com>
2016-06-17 10:11:39 -0700

    Trivial package for travis testing purposes

---
+++ var/spack/repos/builtin/packages/trivial/package.py
@@ -0,0 +1,13 @@
+from spack import *
+
+class Trivial(Package):
+    """Description"""
+
+    homepage = "http://www.example.com"
+
+    version('git', git='https://github.com/DavidPoliakoff/spack-travis.git', branch='master')
+
+    def install(self, spec, prefix):
+        cmake('.',*std_cmake_args)
+        make()
+        make("install")
